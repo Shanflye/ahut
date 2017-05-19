40ad50eaf6795bf5c27f11c616c0bef16dff16fc
shanlei <1376436134@qq.com>
2017-05-19 21:02:06 +0800

    update

--- ahut/js/main.js
+++ ahut/js/main.js
@@ -29,3 +29,51 @@
 	}
 	var timer=setInterval(task,2000);
 }()
+//右侧滚动轮播
++function(){
+	//div  id scroll_aside
+	var div=document.getElementById('scroll_aside');
+	var ul=div.getElementsByTagName('ul')[0];
+	var li=div.getElementsByTagName('li');
+	ul.innerHTML=ul.innerHTML+ul.innerHTML;
+	ul.style.height=li[0].offsetHeight*li.length+'px';
+	var speed=1;
+	function move(){
+		if(ul.offsetTop<-ul.offsetHeight/2){
+			ul.style.top='0';
+		}
+		if(ul.offsetTop>0){
+			ul.style.top=-ul.offsetHeight/2+'px';
+		}
+		ul.style.top=ul.offsetTop-speed+'px';
+	}
+	var timer=setInterval(move,100);
+	div.onmouseover=function(){
+		clearInterval(timer);
+		timer=null;
+	}
+	div.onmouseout=function(){
+		timer=setInterval(move,100)
+	}
+}()
+/*新闻标签页*/
++function(){
+	var lis=document.querySelectorAll('#nav_center li');
+	for(var i=0;i<lis.length;i++){
+		lis[i].onmouseenter=function(){
+			for(var j=0;j<lis.length;j++){
+				lis[j].className='';
+			}
+			this.className='active';
+			var a=this.firstElementChild;
+			var href=a.getAttribute('href');
+			var div=document.getElementById(href);
+			var pages=document.querySelectorAll('#tag_page>div');
+			console.log(pages);
+			for(var i=0;i<pages.length;i++){
+				pages[i].style.display='none';
+			}
+			div.style.display='block';
+		}
+	}
+}()
